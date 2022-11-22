b954f4cc4d03c0ba002e19787b7cb76d5c85f1a8
misha-solntcev <misha.solntcev@gmail.com>
2022-11-22 17:50:48 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            for (int i = 0; i <= 10; i++)
+            {                
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("#");
+                }                
+                Console.WriteLine();
+            }
+            for (int i = 0; i <= 10; i++)
+            {                
+                for (int j = 10; j > i; j--)
+                {
+                    Console.Write("#");
+                }                
+                Console.WriteLine();
+            }
+
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 10; j > i; j--)
+                {
+                    Console.Write(" ");                    
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 10; k > i; k--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }            
+
+            Console.ReadKey();
+        }
+    }
+}
